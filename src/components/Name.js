bb7894b78c41407b8db04af4a17e71d507a96ebf
Jay200123 <kenken.ayonon@gmail.com>
2023-07-12 19:11:07 +0800

    generate random Name

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,15 +1,23 @@
 import { React, useState } from "react"
+import Students from "../data/students"
+import icon from "../images/download.png"
 
 function Name(){
 
-    const [name, setName] = useState("") //sets the current state to null
+    const [name, setName] = useState("Name") //sets the current state to null
 
     function printName(){
-        return setName("RenyelJay") //changes the current State to print a Name using setName function 
+
+        const randomName = Students
+
+        const randomId = Math.floor(Math.random() * randomName.length)
+        const names = randomName[randomId].name
+
+        setName(names)
     }
 
     function clearName(){
-        setName("") //changes the current State to "" or null using setName function 
+        setName("Name") //changes the current State to "" or null using setName function 
     }
 
     // setName returns the name that was being printed inside the parenthesis when the functions are being envoked in the button 
@@ -19,15 +27,14 @@ function Name(){
         <div className="name-container">
 
             <div className="text-container">
-                <h3>Print A Name</h3>
-                <h4>Name: {name}</h4>
-            </div>
-
-            <div className="btn-container"> 
+                <div className="title-container">
+                <img className="title-img" src={icon} alt="React Logo"/>
+                <h3>Generate a Random Name!</h3>
+                </div>
+                <h4 className="text-header">{name}</h4>
+            </div>          
             <button className="btn-form" onClick={printName}>Print Name</button>
-            <button className="btn-form" onClick={clearName}>ClearName</button>
-            </div>
-        
+            <button className="btn-form" onClick={clearName}>Clear Name</button>     
         </div>
     )
 
---
+++ src/data/students.js
@@ -0,0 +1,32 @@
+const Students = [
+
+    {
+        name:"Renyel Jay",
+        course:"Bachelor of Science in Information Technology",
+        section:"BSIT-NS-3A",
+        age:21
+    },
+
+    {
+        name:"Ian Mark",
+        course:"Bachelor of Science in Information Technology",
+        section:"BSIT-NS-3A",
+        age:22
+    },
+
+    {
+        name:"John Neri",
+        course:"Bachelor of Science in Information Technology",
+        section:"BSIT-NS-3A",
+        age:22
+    },
+
+    {
+        name:"Edmon",
+        course:"Bachelor of Science in Information Technology",
+        section:"BSIT-NS-3A",
+        age:21
+    }
+]
+
+export default Students
