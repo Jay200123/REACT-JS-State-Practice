ec96e86d8afbfefeb1ee5dcd8092b5a1aa78650d
Jay200123 <kenken.ayonon@gmail.com>
2023-07-15 21:47:08 +0800

    commit

--- src/components/Ternary.jsx
+++ src/components/Ternary.jsx
@@ -2,26 +2,55 @@ import React, { useState } from "react";
 
 function Ternary(){
 
-    const [isCondition, setCondition] = useState("Your Answer")
+    // const [isCondition, setCondition] = useState("Your Answer")
 
-    function yesClick(){
-        setCondition(true)
+    // function yesClick(){
+    //     setCondition(true)
+    // }
+
+    // function noClick(){
+    //     setCondition(false)
+    // }
+
+    // const [isGoingOut, setGoingOut] = useState(true)
+
+    // function onClick(){
+
+    //     setGoingOut(isGoingOut => !isGoingOut)
+    // }
+
+    const items = ['item1','item2']
+
+    const [item, setItems] = useState(items)
+
+    function addItems(){
+
+        setItems(item => {
+            return[...item, `item${item.length + 1}`]
+        })
     }
-
-    function noClick(){
-        setCondition(false)
-    }
-
 
 
     return(
-        <div className="name-container">
-            <h3>Is State in React Necessary?</h3>
-            <h3>{ isCondition ? "Yes" : "No" }</h3> 
 
-            <button className="btn-form" onClick={yesClick}>Yes</button>
-            <button className="btn-form" onClick={noClick}>No</button>
+        // <div className="name-container">
+        //     <h3>Is State in React Necessary?</h3>
+        //     <h3>{ isGoingOut ? "Yes" : "No" }</h3> 
+
+        //     <button className="btn-form" onClick={onClick}>Answer</button>
+        //     {/* <button className="btn-form" onClick={noClick}>No</button> */}
+        // </div>
+
+        <div className="name-container">
+
+            <div className="text-container">
+               {item}
+            </div>
+
+            <button className="btn-form" onClick={addItems}>Add Items</button>
         </div>
+
+
     )
 }
 
