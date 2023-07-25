5300173872bd86b1c59cdb90e2b988faae119042
Jay200123 <kenken.ayonon@gmail.com>
2023-07-25 22:34:19 +0800

    forms and state

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import { useState } from "react";
 // import Home from "./List/Home"
 
 // List2 Components
-// import "./style/list.css"
+import "./style/list.css"
 // import Home from "./Jokes/Home"
 
 // To Do List Components
@@ -56,16 +56,59 @@ function App() {
 
   // })
 
-  const[messages, setMessages] = useState(["A","B"])
+  // const[messages, setMessages] = useState(["A","B"])
 
 
+  //for first name forms
+  const [name, setName] = useState({
+    fname:"",
+    lname:"",
+    course:""
+
+  })
+
+  console.log(name)
+
+  function handleClick(e){
+
+    setName(prevName=>{
+
+      return {...prevName, [e.target.name]: e.target.value}
+    })
+
+  }
 
   return (
 
 
     <div>
-      {messages.length === 0 ?
-       <h1>Youre all caught up</h1> : <h1>You have {messages.length} unread {messages.length > 1 ? "messages" : "message" }</h1>}
+      <h3>Enter Input</h3>
+
+      <label >First Name:</label>
+      <form>
+        <input type="text" placeholder="Type your First Name" className="form-control" name="fname" onChange={handleClick}/>
+      </form>
+
+      <label>Last Name:</label>
+      <form>
+        <input type="text" placeholder="Type your Last Name" className="form-control" name="lname"  onChange={handleClick}/>
+      </form>
+      
+      <label>Course & Section</label>
+      <form>
+        <input type="text" placeholder="Enter Course and Section" className="form-control" name="course"  onChange={handleClick} />
+      </form>
+
+      <div>
+        <h2>Output</h2>
+        <h3>First Name: {name.fname}</h3>
+        <h3>Last Name: {name.lname}</h3>
+        <h3>Course and Section: {name.course}</h3>
+      </div>
+
+
+      {/* {messages.length === 0 ?
+       <h1>Youre all caught up</h1> : <h1>You have {messages.length} unread {messages.length > 1 ? "messages" : "message" }</h1>} */}
       {/* <Home/> */}
 
       {/* <Main/> */}
