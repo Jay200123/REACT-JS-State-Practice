6ede8f53ce3a5873068746b791b50d91dac034e0
Jay200123 <kenken.ayonon@gmail.com>
2023-07-30 22:14:40 +0800

    commit

--- src/Forms2/Main.jsx
+++ src/Forms2/Main.jsx
@@ -9,14 +9,19 @@ function Main(){
         fname:"",
         middle:"",
         lname:"",
-        comments:""
+        comments:"",
+        isFriendly:true
 
     })
 
     function onClick(e){
 
+        const {name, value, type, checked} = e.target
+
         setForms(prevForms=>{
-            return {...prevForms, [e.target.name]: e.target.value}
+
+            return {...prevForms, [name]: type=== "checkbox" ? checked : value }
+            // return {...prevForms, [e.target.name]: e.target.value}
         })
         
     }
@@ -36,22 +41,27 @@ function Main(){
                 <form>
                     <div>
                         <label>First Name</label>
-                        <input type="text" className="form-control" name="fname" onChange={onClick}  placeholder="Input Text"/>
+                        <input type="text" className="form-control" name="fname" value={forms.fname} onChange={onClick}  placeholder="Input Text"/>
                     </div>
 
                     <div>
                         <label>Middle Name</label>
-                        <input type="text" className="form-control" name="middle" onChange={onClick}  placeholder="Input Text"/>
+                        <input type="text" className="form-control" name="middle" value={forms.middle} onChange={onClick}  placeholder="Input Text"/>
                     </div>
 
                     <div>
                         <label>Last Name</label>
-                        <input type="text" className="form-control" name="lname" onChange={onClick}  placeholder="Input Text"/>
+                        <input type="text" className="form-control" name="lname" value={forms.lname} onChange={onClick}  placeholder="Input Text"/>
                     </div>
 
                     <div>
                         <label>Comments</label>
-                        <textarea className="text-control" name="comments" onChange={onClick}  placeholder="Comments and Suggestions..."/>
+                        <textarea className="text-control" name="comments" value={forms.comments} onChange={onClick}  placeholder="Comments and Suggestions..."/>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" name="isFriendly" checked={forms.isFriendly} onChange={onClick}/>
+                        <label>Are you friendly</label>
                     </div>
                 </form>
             </div>
